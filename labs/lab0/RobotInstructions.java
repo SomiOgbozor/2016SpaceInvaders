0ddbfa47a437fd44547b42f77f8e6f45d12a4bfc
somiogbozor <somiogbozor@somis-mbp.dhcp.wustl.edu>
2016-08-30 15:39:41 -0500

    Committing lab 0. 8/30/16

--- labs/lab0/RobotInstructions.java
+++ labs/lab0/RobotInstructions.java
@@ -29,9 +29,9 @@ public class RobotInstructions  {
 		 */
 		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(223);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(110);
 	}
 
 }
